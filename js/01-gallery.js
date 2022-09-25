c5fc9aa66ca96f7bc45431bf5bf9de1072a6af80
Roman Vanchytskyi <owles@i.ua>
2022-09-25 20:34:56 +0300

    hw-07

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+function createImage(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+</div>`;
+    })
+    .join("");
+}
+
+const imageContainer = document.querySelector(".gallery");
+
+imageContainer.insertAdjacentHTML("beforeend", createImage(galleryItems));
+
+imageContainer.addEventListener("click", openModal);
+
+function openModal(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originImageLink = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src="${originImageLink}">
+  `);
+  instance.show();
+
+  window.addEventListener("keydown", onEscKey);
+
+  function onEscKey(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscKey);
+    }
+  }
+}
